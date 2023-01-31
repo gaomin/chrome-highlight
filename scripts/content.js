d4e2c174be653e2ab82df44166b1059b156188aa
gaomin <gaomin1039@icloud.com>
2023-01-31 17:19:42 +0800

    test

--- scripts/content.js
+++ scripts/content.js
@@ -1,46 +1,68 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-// const store = chrome.storage.local.get('test', (res) =>{
-//     console.log('store', res);
-// });
+const localpath = window.location.href;
+chrome.storage.local.get(localpath, (res) =>{
+    console.log('store', res, res[localpath]);
+    const store = res[localpath];
+    if (store) {
+      const { startOffset, endOffset, innerHTML, xpath} = store;
+      const nodes = document.querySelectorAll(xpath);
+      console.log('nodes',xpath,  nodes, innerHTML);
+      if (nodes.length) {
+        let parent = nodes[0];
+        if (parent.length > 1) {
+            parent = nodes.filter(node => node.innerHTML == innerHTML)[0];
+        } 
+        
+        console.log('parent', parent, parent.innerHTML, innerHTML);
+        // parent.innerHTML = innerHTML;
+        // let range = document.createRange();
+        // range.setStart(parent.firstChild, startOffset);
+        // range.setEnd(parent.firstChild, endOffset);
+  
+        // console.log('range', range);
+        // const newParent = document.createElement("i");
+        // newParent.setAttribute("style", "color:#f00;");
+        // range.surroundContents(newParent);
+      }
+    }
+});
+
 
 document.addEventListener("mouseup", (event) => {
-  // console.log('mouseup', event);
   console.log(window.getSelection(), window.getSelection().getRangeAt(0));
+
   const txt = window.getSelection().toString();
-  console.log(txt);
+  const range = window.getSelection().getRangeAt(0);
+  const localpath = window.location.href;
+  const ele = range.commonAncestorContainer;
+  const innerHTML = ele.innerHTML;
+  console.log('xxxx', innerHTML);
+
   if (txt) {
-    // const parentNode = window.getSelection().focusNode.parentElement;
-    // const html = parentNode.innerHTML.replace(txt, '<span style="color: #f00">' + txt + '</span>');
-    // parentNode.innerHTML = html;
-    const newParent = document.createElement("i");
-    newParent.setAttribute("style", "color:#f00;");
-    var range = window.getSelection().getRangeAt(0);
-    range.surroundContents(newParent);
+    const storeObj = {
+      startOffset: range.startOffset,
+      endOffset: range.endOffset,
+      txt,
+      xpath: '',
+      innerHTML: innerHTML
+    };
+
+    // const newParent = document.createElement("i");
+    // newParent.setAttribute("style", "color:#f00;");
+    // newParent.appendChild(range.extractContents());
+    // range.insertNode(newParent);
 
     const ele = range.commonAncestorContainer;
     const path = getElementXPath(ele);
+    storeObj.xpath = path;
+    // storeObj.innerHTML = ele.innerHTML;
 
-    console.log("xpath", path);
-
-    // chrome.storage.local.remove(['test'])
-    // chrome.storage.local.set({'test': {
-
-    // }}, function() {
-    //     console.log('保存成功！');
-    // });
+    chrome.storage.local.remove([localpath])
+    chrome.storage.local.set({
+      [localpath]: storeObj
+    }, function() {
+      console.log('保存成功！', storeObj);
+    })
   }
 });
 
@@ -85,37 +107,3 @@ var getElementXPath = function (element) {
 
   return paths.length ? " " + paths.join(" ") : null;
 };
-
-// const article = document.querySelector('article');
-
-// `document.querySelector` may return null if the selector doesn't match anything.
-// if (article) {
-//     const text = article.textContent;
-//     /**
-//      * Regular expression to find all "words" in a string.
-//      *
-//      * Here, a "word" is a sequence of one or more non-whitespace characters in a row. We don't use the
-//      * regular expression character class "\w" to match against "word characters" because it only
-//      * matches against the Latin alphabet. Instead, we match against any sequence of characters that
-//      * *are not* a whitespace characters. See the below link for more information.
-//      *
-//      * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-//      */
-//     const wordMatchRegExp = /[^\s]+/g;
-//     const words = text.matchAll(wordMatchRegExp);
-//     // matchAll returns an iterator, convert to array to get word count
-//     const wordCount = [...words].length;
-//     const readingTime = Math.round(wordCount / 200);
-//     const badge = document.createElement('p');
-//     // Use the same styling as the publish information in an article's header
-//     badge.classList.add('color-secondary-text', 'type--caption');
-//     badge.textContent = `⏱️ ${readingTime} min read`;
-
-//     // Support for API reference docs
-//     const heading = article.querySelector('h1');
-//     // Support for article docs with date
-//     const date = article.querySelector('time')?.parentNode;
-
-//     // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
-//     (date ?? heading).insertAdjacentElement('afterend', badge);
-// }
